52097e9040b24a3fc9cef702820d933f671546db
KavinDon <85749329+KavinDon@users.noreply.github.com>
2021-08-21 13:24:23 +0530

    commit

---
+++ file_creation.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class file_creation {
+    public static void main(String[] args) {
+        boolean present;
+        File file = new File("E:\\samplefile\\note.txt");
+    }
+}
